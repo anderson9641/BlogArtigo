76ef69f738ada4d9b487713787a4e7d49ef67b9b
Anderson Alves <anderson96627210@gmail.com>
2020-12-21 18:15:43 -0300

    pegar imagem do perfil do usuario

--- src/main/java/com/student/entities/User.java
+++ src/main/java/com/student/entities/User.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -49,6 +50,7 @@ public class User implements Serializable {
 	
 	@Getter
 	@Setter
+	@Lob
 	private byte[] imageProfile;
 	
 	@Getter
--- src/main/java/com/student/resources/UserResource.java
+++ src/main/java/com/student/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.student.resources;
 
+import java.io.IOException;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.student.entities.User;
@@ -28,7 +31,14 @@ public class UserResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody User obj) {
+	public ResponseEntity<Void> insert(@RequestBody User obj, 
+			@RequestParam(" name do input Component que recebe a imagem") MultipartFile imagem) {
+		try {
+			obj.setImageProfile(imagem.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 			.path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -48,6 +58,12 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/Image/{id}",method = RequestMethod.GET)
+	public ResponseEntity<byte[]> findByIdImage(@PathVariable Long id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getImageProfile());	
 	}
 	
 
